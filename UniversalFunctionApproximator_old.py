313cdfe3a9b1ebd4a954bf200eb2e189d694747f
latuannetnam <latuannetnam@gmail.com>
2017-05-26 17:43:14 +0700

    change to multiple hidden layer

--- UniversalFunctionApproximator_old.py
+++ UniversalFunctionApproximator_old.py
@@ -27,9 +27,14 @@ class UniversalFunctionApproximator:
         self.sess = tf.Session(config=tf.ConfigProto(
             intra_op_parallelism_threads=UniversalFunctionApproximator.NUM_THREADS))
         self.logs_path = UniversalFunctionApproximator.LOG_ROOT_DIR + "ufa"
-
         self.input_X = input_X
         self.label_Y = label_Y
+        # feature scaling to increase convergence
+        # standard_scaler = MinMaxScaler()
+        # print("X-pre-scale:", min(input_X), max(input_X))
+        # input_X = standard_scaler.fit_transform(input_X)
+        # print("X-scale:", min(input_X), max(input_X))
+        # label_Y = standard_scaler.fit_transform(label_Y)
 
         # number of neurals per hidden layer1, if n_neurals<2: use simple linear
         # regression
@@ -43,50 +48,26 @@ class UniversalFunctionApproximator:
         self.batch_size = batch_size  # batch size for training
         if batch_size >= self.train_size:
             self.batch_size = self.train_size
+        #  unfix, set batch_size = train_size to increase convergence
+        # self.batch_size = self.train_size
+
         self.epochs = epochs  # total number of training loops
+        self.train_X = input_X[:self.train_size]  # train data
+        self.test_X = input_X[self.train_size:]   # test data
+        self.train_Y = label_Y[:self.train_size]  # train label
+        self.test_Y = label_Y[self.train_size:]   # test label
         # number of features of train data: X1, X2 ...
         self.n_features = input_X.shape[1]
-
-        # preload input into queue
-        self.preload()
-        # self.train_X = input_X[:self.train_size]  # train data
-        # self.test_X = input_X[self.train_size:]   # test data
-        # self.train_Y = label_Y[:self.train_size]  # train label
-        # self.test_Y = label_Y[self.train_size:]   # test label
-
-        # with tf.variable_scope('Input', reuse=False):  # place holder for Input
-        #     self.X = tf.placeholder("float", shape=(
-        #         None, self.n_features), name="X")
-        # with tf.variable_scope('Label', reuse=False):  # place holder for Label
-        #     self.Y = tf.placeholder("float", shape=(None), name='Y')
+        with tf.variable_scope('Input', reuse=False):  # place holder for Input
+            self.X = tf.placeholder("float", shape=(
+                None, self.n_features), name="X")
+        with tf.variable_scope('Label', reuse=False):  # place holder for Label
+            self.Y = tf.placeholder("float", shape=(None), name='Y')
         if self.n_neurals < 2:  # use linear regression
             with tf.variable_scope("Weights", reuse=False):
                 self.W = tf.Variable(
                     tf.zeros([self.n_features, 1]), name="weights")
                 self.b = tf.Variable(tf.zeros([1]), name="bias")
-
-    def preload(self):  # preload data into queue
-        with tf.name_scope('preload'):
-            print("X dtype:", self.input_X.dtype)
-            # Input data
-            self.x_initializer = tf.placeholder(
-                self.input_X.dtype,
-                shape=(self.input_X.shape[0], self.n_features))
-            self.labels_initializer = tf.placeholder(
-                self.label_Y.dtype,
-                shape=(self.label_Y.shape[0], 1))
-            self.preload_X = tf.Variable(
-                self.x_initializer, trainable=False, collections=[])
-            self.preload_Y = tf.Variable(
-                self.labels_initializer, trainable=False, collections=[])
-
-            self.input, self.label = tf.train.slice_input_producer(
-                [self.preload_X, self.preload_Y], num_epochs=self.epochs)
-
-            self.X, self.Y = tf.train.batch(
-                [self.input, self.label],
-                batch_size=self.batch_size,
-                allow_smaller_final_batch=True)
 
     def __del__(self):
         self.coord.request_stop()
@@ -94,11 +75,11 @@ class UniversalFunctionApproximator:
         self.sess.close()
 
     def dump_input(self):
-        train = np.c_[self.input_X, self.label_Y]
+        train = np.c_[self.train_X, self.train_Y]
         print("Train size:", train.shape[0])
         # print(train)
-        # test = np.c_[self.test_X, self.test_Y]
-        # print("test size:", test.shape[0])
+        test = np.c_[self.test_X, self.test_Y]
+        print("test size:", test.shape[0])
         # print(test)
         print("Number of features:", self.n_features)
         print("Number of hidden layers:",
@@ -107,14 +88,14 @@ class UniversalFunctionApproximator:
               str(self.batch_size) +
               " alpha:" + str(self.learning_rate))
 
-    def inference(self, X, reuse=False):   # define neural network with 1 hidden layer
+    def inference(self, reuse=False):   # define neural network with 1 hidden layer
         if self.n_neurals < 2:  # use linear regression
             with tf.variable_scope('Model', reuse=reuse):
-                model = tf.add(tf.matmul(X, self.W), self.b)
+                model = tf.add(tf.matmul(self.X, self.W), self.b)
         else:  # use neural network
             with tf.variable_scope('Neural_Net', reuse=reuse):
                 hidden1_layer = tf.layers.dense(
-                    X, self.n_neurals, activation=tf.nn.tanh,
+                    self.X, self.n_neurals, activation=tf.nn.sigmoid,
                     name="Hidden1")
                 hidden2_layer = tf.layers.dense(
                     hidden1_layer, self.n_neurals, activation=tf.nn.relu6,
@@ -127,7 +108,7 @@ class UniversalFunctionApproximator:
         return model
 
     def loss(self, reuse=False):
-        Y_predicted = self.inference(self.X)
+        Y_predicted = self.inference()
         logits = tf.log(Y_predicted)
         with tf.variable_scope("Loss", reuse=reuse):
             # cost = tf.reduce_sum(tf.squared_difference(
@@ -146,7 +127,8 @@ class UniversalFunctionApproximator:
 
     def plot(self, show=True):
         # Graphic display
-        # predicted = self.sess.run(self.inference(reuse=True))
+        predicted = self.sess.run(self.inference(reuse=True),
+                                  feed_dict={self.X: self.input_X})
         title = 'Epochs:' + str(self.epochs) + \
             " batch:" + str(self.batch_size) + \
             " layers:" + str(self.n_layers) + \
@@ -158,9 +140,7 @@ class UniversalFunctionApproximator:
             # plt.plot(self.train_X, predicted, c='r', label='Fitted')
             plt.scatter(self.input_X, self.label_Y,
                         c='b', label='Original data')
-            print(self.input_X.size)
-            print(self.predicted.size)            
-            plt.scatter(self.input_X, self.predicted, c='r', label='Fitted')
+            plt.scatter(self.input_X, predicted, c='r', label='Fitted')
 
             plt.legend()
         else:  # plot 3D for X1, X2
@@ -173,15 +153,15 @@ class UniversalFunctionApproximator:
             # ax = fig.add_subplot(111, projection='3d')
             ax = fig.gca(projection='3d')
             ax.text2D(0.05, 0.95, title, transform=ax.transAxes)
-            # x = np.squeeze(np.asarray(train_X1))
-            # y = np.squeeze(np.asarray(train_X2))
-            # z = np.squeeze(np.asarray(self.label_Y))
-            # z1 = np.squeeze(np.asarray(self.predicted))
+            x = np.squeeze(np.asarray(train_X1))
+            y = np.squeeze(np.asarray(train_X2))
+            z = np.squeeze(np.asarray(self.label_Y))
+            z1 = np.squeeze(np.asarray(predicted))
             # ax.plot(x, y, zs=z, c='b', label='Original data')
             # ax.plot(x, y, zs=z1, c='r', label='Fitted')
             ax.scatter(train_X1, train_X2, self.label_Y, c='b',
                        marker='s', label='Original data')
-            ax.scatter(train_X1, train_X2, self.predicted,
+            ax.scatter(train_X1, train_X2, predicted,
                        c='r', marker='v', label='Fitted')
             ax.set_xlabel('X1')
             ax.set_ylabel('X2')
@@ -217,6 +197,22 @@ class UniversalFunctionApproximator:
         image_summary = self.sess.run(image_summary_op)
         self.summary_writer.add_summary(image_summary)
 
+    def iterate_minibatches(self, shuffle=False):
+        inputs = self.train_X
+        targets = self.train_Y
+        batchsize = self.batch_size
+        assert inputs.shape[0] == targets.shape[0]
+        if shuffle:
+            indices = np.arange(inputs.shape[0])
+            np.random.shuffle(indices)
+            # print(indices)
+        for start_idx in range(0, inputs.shape[0] - batchsize + 1, batchsize):
+            if shuffle:
+                excerpt = indices[start_idx:start_idx + batchsize]
+            else:
+                excerpt = slice(start_idx, start_idx + batchsize)
+            yield inputs[excerpt], targets[excerpt]
+
     def fit_model(self):  # run loop to train model
         total_loss = self.loss()
         # Create a summary to monitor cost tensor
@@ -224,16 +220,9 @@ class UniversalFunctionApproximator:
         # Merge all summaries into a single op
         merged_summary_op = tf.summary.merge_all()
         train_op = self.train(total_loss), total_loss, merged_summary_op
-        init = [tf.global_variables_initializer(
-        ), tf.local_variables_initializer()]
+        init = tf.global_variables_initializer()
         # Launch the graph in a session, setup boilerplate
         self.sess.run(init)
-        self.sess.run(self.preload_X.initializer,
-                      feed_dict={self.x_initializer: self.input_X})
-        self.sess.run(self.preload_Y.initializer,
-                      feed_dict={self.labels_initializer: self.label_Y})
-        print(self.preload_X.get_shape())
-        print(self.preload_Y.get_shape())
         self.coord = tf.train.Coordinator()
         self.threads = tf.train.start_queue_runners(
             sess=self.sess, coord=self.coord)
@@ -242,37 +231,37 @@ class UniversalFunctionApproximator:
             self.logs_path, graph=tf.get_default_graph())
 
         # actual training loop
-        try:
-            loss = 0
-            print_step = self.epochs // 10
-            step = 0
-            Y_predicted = []
-            while not self.coord.should_stop():
-                # Run one step of the model.
-                result = self.sess.run([train_op])
+        loss = 0
+        print_step = self.epochs // 10
+        for step in range(self.epochs):  # run all data
+            if self.batch_size >= self.train_size:
+                result = self.sess.run([train_op],
+                                       feed_dict={self.X: self.train_X,
+                                                  self.Y: self.train_Y})
                 loss = result[0][1]
                 summary = result[0][2]
                 # Write logs at every iteration
                 self.summary_writer.add_summary(summary, step + 1)
-                if step % print_step == 0:
-                    print("step:", step + 1, " loss: ", loss)
-                step += 1
-        except tf.errors.OutOfRangeError:
-            print('Done training for:', step, " epochs")
-        finally:
-            # When done, ask the threads to stop.
-            print(" final loss:", loss)
-            predicted = self.inference(tf.stack(self.input_X), reuse=True)
-            self.sess.run(predicted)
-            self.predicted = predicted.eval(self.sess)
-            print(predicted.size)
-            self.save_image()
+            else:  # run in batch_size of data
+                for batch in self.iterate_minibatches(shuffle=True):
+                    x_batch, y_batch = batch
+                    result = self.sess.run([train_op],
+                                           feed_dict={self.X: x_batch,
+                                                      self.Y: y_batch})
+                    loss = result[0][1]
+                    summary = result[0][2]
+                    # Write logs at every iteration
+                    self.summary_writer.add_summary(summary, step + 1)
 
-        # predicted = 0
-        # try:  # run predict
-        #     predicted = self.sess.run(self.inference(reuse=True))
+            if step % print_step == 0:
+                loss = self.sess.run([total_loss],
+                                     feed_dict={self.X: self.train_X,
+                                                self.Y: self.train_Y})
+                print("step:", step + 1, " loss: ", loss)
 
-        # except tf.errors.OutOfRangeError:
-        #     print('Done predicted for:')
-        # finally:
-        #     print(predicted.get_shape())
+        print(" final loss:",
+              self.sess.run([total_loss],
+                            feed_dict={self.X: self.train_X,
+                                       self.Y: self.train_Y}))
+
+        self.save_image()
--- test_ufa.py
+++ test_ufa.py
@@ -4,34 +4,54 @@ import random
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from UniversalFunctionApproximator import UniversalFunctionApproximator
 
-NUM_HIDDEN_NODES = 100
-NUM_EXAMPLES = 1000
+NUM_EXAMPLES = 100
 TRAIN_SPLIT = .8
-MINI_BATCH_SIZE = 100
-NUM_EPOCHS = 10000
-choice = 1
+
+choice = 3
 # train_X0 = np.array([np.linspace(-10, 10, 50)]).T
 train_X0 = np.float32(
     np.random.uniform(math.pi, 6 * math.pi, (1, NUM_EXAMPLES))).T
-print("X0:", min(train_X0), max(train_X0))
-# standard_scaler = MinMaxScaler()
-# train_X0 = standard_scaler.fit_transform(train_X0)
-print("X0-scale:", min(train_X0), max(train_X0))
+
 
 if choice == 1:  # test set 1
-    train_Y = np.cos(train_X0) * np.sin(train_X0) - 7
+    # best paramaters for function
+    # single hidden layer, activation = tanh
+    NUM_LAYERS = 1
+    NUM_HIDDEN_NODES = 500
+    MINI_BATCH_SIZE = 10
+    NUM_EPOCHS = 80000
+    LEARNING_RATE = 0.005
+    train_Y = np.cos(train_X0) * np.sin(train_X0)
     train_X = np.c_[train_X0]
-    # print("X:", min(train_X0), max(train_X0))
-    # print("Y", min(train_Y), max(train_Y))
+
+
 elif choice == 2:  # test set 2
+    NUM_LAYERS = 1
+    NUM_HIDDEN_NODES = 500
+    MINI_BATCH_SIZE = 10
+    NUM_EPOCHS = 200000
+    LEARNING_RATE = 0.001
+
     train_X1 = train_X0
     train_X2 = train_X0 * train_X0
     train_Y = np.sin(train_X1 + train_X2)
     train_X = np.c_[train_X1, train_X2]
 
+elif choice == 3:  # test set 3
+    # best paramaters for function
+    NUM_LAYERS = 5
+    NUM_HIDDEN_NODES = 500
+    MINI_BATCH_SIZE = 20
+    NUM_EPOCHS = 100
+    LEARNING_RATE = 0.005
+    train_Y = np.cos(train_X0) * np.sin(train_X0)
+    train_X = np.c_[train_X0]
 
 ufa = UniversalFunctionApproximator(train_X, train_Y,
-                                    NUM_HIDDEN_NODES, 0.01, NUM_EPOCHS, MINI_BATCH_SIZE)
+                                    NUM_LAYERS, NUM_HIDDEN_NODES,
+                                    LEARNING_RATE,
+                                    NUM_EPOCHS, MINI_BATCH_SIZE,
+                                    TRAIN_SPLIT)
 ufa.dump_input()
 ufa.fit_model()
 ufa.plot()
